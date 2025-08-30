540b7c0348c7580f9543985adb5d2dd6734df4c0
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-08-30 22:13:30 +0530

    all done

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2
 const {CloudinaryStorage} =require('multer-storage-cloudinary');
 const multer=require('multer');
 const path=require("path");
-
+require('dotenv').config()
 cloudinary.config({
     cloud_name:process.env.NAME,
     api_key:process.env.API_KEY,
